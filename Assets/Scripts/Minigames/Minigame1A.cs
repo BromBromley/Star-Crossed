dcc832006c67c381dba995678c40df84ec7186f6
Mia Wittenmeier <mia.wittenmeier@students.hs-mainz.de>
2024-04-30 10:34:08 +0200

    rearrange minigame hierarchy; add some UI assets and set up next minigame (wip)

---
+++ Assets/Scripts/Minigames/Minigame1A.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minigame1A : MonoBehaviour
+{
+    // this script manages the minigame "fixing the spacesuit"
+
+    [SerializeField] private GameObject scanner;
+    private bool showingScanner;
+    private bool usingScanner;
+}
